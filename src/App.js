e720f12cf9c3fd7732ba0d4a33a927c282ff6e8f
stunter777 <102242794+stunter777@users.noreply.github.com>
2023-02-22 23:22:14 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [active, setActive] = useState("");
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      play(event.key.toUpperCase());
+    });
+  }, []);
+  const pads = [
+    {
+      keyCode: 81,
+      text: "Q",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      keyCode: 87,
+      text: "W",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      keyCode: 69,
+      text: "E",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      keyCode: 65,
+      text: "A",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      keyCode: 83,
+      text: "S",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      keyCode: 68,
+      text: "D",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      keyCode: 90,
+      text: "Z",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      keyCode: 88,
+      text: "X",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      keyCode: 67,
+      text: "C",
+      src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+  function play(selector) {
+    const audio = document.getElementById(selector);
+    audio.play();
+    setActive(selector);
+  }
+  return (
+    <div className="App">
+      <div id="drum-machine">
+        <div id="display">{active}</div>
+        <div className="drum-pads">
+          {pads.map((pad) => (
+            <div
+              key={pad.src}
+              onClick={() => {
+                play(pad.text);
+              }}
+              className="drum-pad"
+              id={pad.src}
+            >
+              {pad.text}
+              <audio className="clip" id={pad.text} src={pad.src}></audio>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
